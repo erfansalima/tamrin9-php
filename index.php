980bcbadd42dd1373476620936d27c1c6740ea90
erfansalima <erfansalima@gmail.com>
2021-12-16 18:10:27 +0330

    index updated

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
-$question = 'این یک پرسش نمونه است';
-$msg = 'این یک پاسخ نمونه است';
-$en_name = 'hafez';
-$fa_name = 'حافظ';
+$message_file = file_get_contents('messages.txt');
+$message_text = explode(PHP_EOL,$message_file);
+$name_file=file_get_contents('people.json');
+$name_array= json_decode($name_file,true);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $en_name = $_POST['person'];
+    $fa_name=$name_array[$en_name];
+    $question=$_POST['question'];
+    $msg=$message_text[(intval(hash('md5', $en_name.$question),10) % 16)];
+    $first = "/^آیا/iu";
+    $last1 = "/\?$/i";
+    $last2 = "/؟$/u";
+    if (!(preg_match($last1, $question)||preg_match($last2, $question))){
+            $msg="سوال درستی پرسیده نشده";}
+         
+    if (!preg_match($first, $question)){
+            $msg="سوال درستی پرسیده نشده";}}      
+else {
+    $msg = "سوال خود را بپرس!";
+    $question = '';
+    $en_name = array_rand($name_array);
+    $fa_name = $name_array[$en_name];
+}
+if (empty($question)){
+    $msg="سوال خود را بپرس!";
+    $label_val='';}
+
+else {
+       $label_val='پرسش:';}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +43,7 @@ $fa_name = 'حافظ';
 <p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
 <div id="wrapper">
     <div id="title">
-        <span id="label">پرسش:</span>
+        <span id="label"><?php echo $label_val ?></span>
         <span id="question"><?php echo $question ?></span>
     </div>
     <div id="container">
@@ -36,12 +64,16 @@ $fa_name = 'حافظ';
             را از
             <select name="person">
                 <?php
-                /*
-                 * Loop over people data and
-                 * enter data inside `option` tag.
-                 * E.g., <option value="hafez">حافظ</option>
-                 */
-                ?>
+               $input=file_get_contents('people.json');
+                        $names=json_decode($input);
+                            foreach($names as $key => $value){
+                                if ($key!=$en_name){
+                               echo "<option value=$key> $value </option>";}
+                                    
+                                else{
+                               echo "<option value=$key selected> $value </option>";}
+                            }
+                 ?>
             </select>
             <input type="submit" value="بپرس"/>
         </form>
